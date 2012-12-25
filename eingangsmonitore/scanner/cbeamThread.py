a02d80470cc6ceddc2d9e2cbbec3698b1c77b218
sMiLe <smile@c3pb.de>
2012-12-25 18:33:30 +0100

    changed to django-json-rpc

--- eingangsmonitore/scanner/cbeamThread.py
+++ eingangsmonitore/scanner/cbeamThread.py
@@ -1,4 +1,5 @@
 import threading, jsonrpclib
+from jsonrpc.proxy import ServiceProxy
 from time import sleep
 
 
@@ -6,16 +7,17 @@ class cbeamThread ( threading.Thread ):
 
     def __init__(self, cbeamurl):
         threading.Thread.__init__(self) 
-        self.cbeam = jsonrpclib.Server(cbeamurl)
+        self.cbeam = ServiceProxy(cbeamurl)
         self.cbeamdata = {}
 
     def run (self):
         while True:
-            tmp = self.cbeam.who()
-            events = self.cbeam.events()
+            tmp = self.cbeam.who()['result']
+            events = self.cbeam.events()['result']
             tmp['events'] = events
             self.cbeamdata = tmp
             sleep(10)
+            print "c-beam data updated."
     def getcbeamdata(self):
         print self.cbeamdata
         return self.cbeamdata
